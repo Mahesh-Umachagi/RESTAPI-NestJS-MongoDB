bbeec3ca1bda3895563102582b7fd6f7d7b0741a
Mahesh Umachagi <Mahesh-Umachagi@github.com>
2020-04-28 18:39:52 +0530

    created end points

---
+++ src/items/items.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+
+@Controller('items')
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'Get all items';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `Update ${id}-Name: ${updateItemDto.name}`;
+  }
+}
